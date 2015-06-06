db55c9cfb48841072e23a66b2c9ce9b13a15109f
Tim Sell <trsell@gmail.com>
2015-06-06 19:59:23 -0400

    wrap hurdles up to take the handlers as an argument

--- spec/hurdles_spec.js
+++ spec/hurdles_spec.js
@@ -1,26 +1,29 @@
-var hurdles = require('../src/hurdles');
+var hurdlesFactory = require('../src/hurdles');
 var _ = require('lodash');
 
 describe('hurdles', function () {
   var output;
   var asyncError;
-
   var foo = {a: 1, b: 2};
   var bar = {x: 3, y: 4};
+
+  function handlers(typeToObjectMap) {
+    var handlers = {};
+    _.each(typeToObjectMap, function (v, k) {
+      handlers[k] = function () {
+        return Promise.resolve(v);
+      };
+    });
+    return handlers;
+  };
 
   beforeEach(function () {
     output = null;
     asyncError = null;
-    hurdles.registerHandler('foo', function () {
-      return Promise.resolve(foo);
-    });
-    hurdles.registerHandler('bar', function () {
-      return Promise.resolve(bar);
-    });
   });
 
 
-  function runQuery(q, done) {
+  function runQuery(hurdles, q, done) {
     hurdles.query(q).then(function (o) {
       output = o;
       done()
@@ -30,19 +33,35 @@ describe('hurdles', function () {
     });
   };
 
-  describe('happy day query', function () {
+  describe('querying with simple object', function () {
     beforeEach(function (done) {
-      runQuery({foo: {a: null, b: null}}, done);
+      var hurdles = hurdlesFactory(handlers({foo: foo}));
+      var query = {foo: {a: null, b: null}};
+      runQuery(hurdles, query, done);
     });
-    it("all nulls replaced with value", function () {
+    it("has same shape and filled values", function () {
       if (asyncError) throw asyncError;
-      expect(output).toEqual({foo: foo});
+      expect(output).toEqual({foo: {a: foo.a, b: foo.b}});
+    });
+  });
+
+  describe('query with no matching handler', function () {
+    beforeEach(function (done) {
+      var hurdles = hurdlesFactory(handlers({}));
+      var query = {foo: {a: null, b: null}};
+
+      runQuery(hurdles, query, done);
+    });
+
+    it("throws error", function () {
+      expect(asyncError.message).toMatch(/unhandled type/);
     });
   });
 
   describe('query for some fields', function () {
     beforeEach(function (done) {
-      runQuery({foo: {a: null}}, done);
+      var hurdles = hurdlesFactory(handlers({foo: foo}));
+      runQuery(hurdles, {foo: {a: null}}, done);
     });
     it("selects only some fields", function () {
       if (asyncError) throw asyncError;
@@ -50,9 +69,22 @@ describe('hurdles', function () {
     });
   });
 
-  describe('nested query', function () {
+  describe('query for nested shape filled by one handler', function () {
+    var expected = {
+      foo: {
+        a: foo.a,
+        b: foo.b,
+        bar: {
+          x: bar.x,
+          y: bar.y
+        }
+      }
+    }
     beforeEach(function (done) {
-      runQuery({
+      var hurdles = hurdlesFactory(handlers({
+        foo: expected.foo
+      }));
+      runQuery(hurdles, {
         foo: {
           a: null,
           b: null,
@@ -64,7 +96,28 @@ describe('hurdles', function () {
       }, done);
     });
 
-    it("selects nested object", function () {
+    it("fills nested shape using one handler", function () {
+      if (asyncError) throw asyncError;
+      expect(output).toEqual(expected);
+    });
+  });
+
+  describe('query with nested shape that root handler does not fill', function () {
+    beforeEach(function (done) {
+      var hurdles = hurdlesFactory(handlers({foo: foo, bar: bar}));
+      runQuery(hurdles, {
+        foo: {
+          a: null,
+          b: null,
+          bar: {
+            x: null,
+            y: null
+          }
+        }
+      }, done);
+    });
+
+    it("fills nested shape using handler", function () {
       if (asyncError) throw asyncError;
       var expected = {
         foo: {
--- src/hurdles.js
+++ src/hurdles.js
@@ -22,150 +22,174 @@ var _ = require('lodash');
 //  }
 //};
 
-function QueryException(message, query, type) {
-  self = this;
-  _.assign(this, {
-    message: message,
-    query: query,
-    type: type
-  });
-}
+function hurdles(handlers) {
 
-function pickFields(output, query, type) {
-  var expectedFields = _.filter(_.keys(query), function (key) {
-    return query[key] == null
-  });
-  _.each(expectedFields, function (key) {
-    if (!output[key]) {
-      throw new QueryException('Query output for ' + type + ' is missing field: ' + key + '. Got: '
-        + JSON.stringify(output), query, type);
-    }
-  });
-  return _.pick(output, expectedFields);
-}
 
-function handleQuery(query, type, upstream) {
-  var inputs = _.isArray(query) ? getInputs(query[0]) : getInputs(query);
-  _.assign(inputs, upstream || {});
+  function QueryException() {
+    var tmp = Error.apply(this, arguments);
+    tmp.name = this.name = 'QueryException'
 
-  if (!handlers[type]) {
-    throw new QueryException('Querying for unknown type', query, type);
+    this.message = tmp.message;
+    /*this.stack = */Object.defineProperty(this, 'stack', { // getter for more optimizy goodness
+      get: function() {
+        return tmp.stack
+      }
+    });
+    return this
+  }
+  var IntermediateInheritor = function() {}
+  IntermediateInheritor.prototype = Error.prototype;
+  QueryException.prototype = new IntermediateInheritor();
+
+  function QueryException(message, query, type) {
+    self = this;
+    _.assign(this, {
+      message: message,
+      query: query,
+      type: type
+    });
   }
 
-  console.log('handling query type =', type, ', inputs =', inputs, ', query =', query);
-  return handlers[type](inputs, query).then(function (output) {
-    if (_.isPlainObject(query) && !_.isPlainObject(output)) {
-      throw new QueryException('Query output mismatch. Expected plain object, got '
-        + JSON.stringify(output), query, type);
-    } else if (_.isArray(query) && !_.isArray(output)) {
-      throw new QueryException('Query output mismatch. Expected array, got '
-        + JSON.stringify(output), query, type);
+  function pickFields(output, query, type) {
+    var expectedFields = _.filter(_.keys(query), function (key) {
+      return !_(key).startsWith('_');
+    });
+    //_.each(expectedFields, function (key) {
+    //  if (!output[key]) {
+    //    throw new QueryException('Query output for ' + type + ' is missing field ' + key + '. Got '
+    //      + JSON.stringify(output));
+    //  }
+    //});
+    return _.pick(output, expectedFields);
+  }
+
+  function handleQuery(query, type, upstream) {
+    var inputs = _.isArray(query) ? getInputs(query[0]) : getInputs(query);
+    _.assign(inputs, upstream || {});
+
+    if (!handlers[type]) {
+      throw new QueryException('Querying for unhandled type ' + type + ' with query ' + JSON.stringify(query));
     }
 
-    if (_.isPlainObject(output)) {
-      output = pickFields(output, query, type);
-    }
+    console.log('handling query type =', type, ', inputs =', inputs, ', query =', query);
+    return handlers[type](inputs, query).then(function (output) {
+      if (_.isPlainObject(query) && !_.isPlainObject(output)) {
+        throw new QueryException('Query output mismatch. Expected plain object, got '
+          + JSON.stringify(output) + ' for type ' + type + ' and query ' + JSON.stringify(query));
+      } else if (_.isArray(query) && !_.isArray(output)) {
+        throw new QueryException('Query output mismatch. Expected array, got '
+          + JSON.stringify(output) + ' for type ' + type + ' and query ' + JSON.stringify(query));
+      }
 
-    if (_.isArray(output)) {
-      output = _.map(output, function(out) {
-        return pickFields(out, query[0], type);
+      if (_.isPlainObject(output)) {
+        output = pickFields(output, query, type);
+      }
+
+      if (_.isArray(output)) {
+        output = _.map(output, function (out) {
+          return pickFields(out, query[0], type);
+        });
+      }
+
+      return output;
+    });
+
+  };
+
+  function buildQueryPromises(query, type, parent) {
+    var plan = {
+      parent: parent,
+      query: query,
+      type: type,
+      promiseFactory: type ? function (upstream) {
+        return handleQuery(query, type, upstream);
+      } : function () {
+        return Promise.resolve({});
+      }
+    };
+    if (_.isArray(query)) {
+      plan.subQueries = _.map(getQueries(query[0]), function (subQuery, subType) {
+        return buildQueryPromises(subQuery, subType, plan);
+      });
+    } else if (_.isPlainObject(query)) {
+      plan.subQueries = _.map(getQueries(query), function (subQuery, subType) {
+        return buildQueryPromises(subQuery, subType, plan);
       });
     }
+    return plan;
+  }
 
-    return output;
-  });
+  function runSubPlans(plan, output, _upstream) {
+    var upstream = {};
+    upstream[plan.type] = _.cloneDeep(output);
+    _.assign(upstream[plan.type], _upstream || {});
 
-};
-
-function buildQueryPromises(query, type, parent) {
-  var plan = {
-    parent: parent,
-    query: query,
-    type: type,
-    promiseFactory: type ? function (upstream) {
-      return handleQuery(query, type, upstream);
-    } : function() {
-      return Promise.resolve({});
-    }
-  };
-  if (_.isArray(query)) {
-    plan.subQueries = _.map(getQueries(query[0]), function (subQuery, subType) {
-      return buildQueryPromises(subQuery, subType, plan);
+    return Promise.all(_.map(plan.subQueries, function (subPlan) {
+      console.log('subplan type', subPlan.type, 'output', output);
+      if (output[subPlan.type]) {
+        return Promise.resolve(output[subPlan.type]);
+      } else {
+        return runPlan(subPlan, upstream);
+      }
+    })).then(function (subOutputs) {
+      var mergedOutput = output;
+      _.each(_.zipObject(_.map(plan.subQueries, 'type'), subOutputs), function (subOutput, subPlanType) {
+        mergedOutput[subPlanType] = subOutput;
+      });
+      return mergedOutput;
     });
-  } else if (_.isPlainObject(query)) {
-    plan.subQueries = _.map(getQueries(query), function (subQuery, subType) {
-      return buildQueryPromises(subQuery, subType, plan);
+
+  }
+
+  function runPlan(plan, upstream) {
+    //console.log('running plan', plan.type, plan.query);
+
+    return plan.promiseFactory(upstream).then(function (output) {
+      if (_.isArray(output)) {
+        return Promise.all(_.map(output, function (out) {
+          return runSubPlans(plan, out, upstream);
+        }));
+      } else {
+        return runSubPlans(plan, output, upstream);
+      }
     });
   }
-  return plan;
-}
 
-function runSubPlans(plan, output, _upstream) {
-  var upstream = {};
-  upstream[plan.type] = _.cloneDeep(output);
-  _.assign(upstream[plan.type], _upstream || {});
-
-  return Promise.all(_.map(plan.subQueries, function(subPlan) {
-    return runPlan(subPlan, upstream);
-  })).then(function(subOutputs) {
-    var mergedOutput = output;
-    _.each(_.zipObject(_.map(plan.subQueries, 'type'), subOutputs), function (subOutput, subPlanType) {
-      mergedOutput[subPlanType] = subOutput;
+  function getInputs(query) {
+    var inputs = {};
+    _.each(_.pick(query, _.filter(_.keys(query), function (a) {
+      return _(a).startsWith('_')
+    })), function (val, key) {
+      inputs[key.match(/_(.*)/)[1]] = val;
     });
-    return mergedOutput;
-  });
+    return inputs;
+  }
 
-}
+  function getQueries(query) {
+    return _.pick(query, _.filter(_.keys(query), function (key) {
+      return !_(key).startsWith('_') && (_.isPlainObject(query[key]) || _.isArray(query[key]));
+    }))
+  }
 
-function runPlan(plan, upstream) {
-  console.log('running plan', plan.type, plan.query);
+  return {
+    QueryException: QueryException,
 
-  return plan.promiseFactory(upstream).then(function (output) {
-    if (_.isArray(output)) {
-      return Promise.all(_.map(output, function(out) {
-        return runSubPlans(plan, out, upstream);
-      }));
-    } else {
-      return runSubPlans(plan, output, upstream);
-    }
-  });
-}
-
-function getInputs(query) {
-  var inputs = {};
-  _.each(_.pick(query, _.filter(_.keys(query), function (a) {
-    return _(a).startsWith('_')
-  })), function (val, key) {
-    inputs[key.match(/_(.*)/)[1]] = val;
-  });
-  return inputs;
-}
-
-function getQueries(query) {
-  return _.pick(query, _.filter(_.keys(query), function (key) {
-    return !_(key).startsWith('_') && (_.isPlainObject(query[key]) || _.isArray(query[key]));
-  }))
-}
-
-var handlers = {};
-
-module.exports = {
-  QueryException: QueryException,
-
-  registerHandler: function (type, handler) {
-    handlers[type] = handler;
-  },
-
-  query: function (query, type) {
-    try {
-      var plan = buildQueryPromises(query, type, null);
-      return runPlan(plan);
-    } catch (e) {
-      console.log(e);
-      throw e;
+    query: function (query, type) {
+      try {
+        var plan = buildQueryPromises(query, type, null);
+        return runPlan(plan);
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
     }
   }
+}
+
+module.exports = function (handlers) {
+  return hurdles(handlers);
 };
+
 
 // TODO: TESTS!
 // TODO:
