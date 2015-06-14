86b6a170c155c0ae159bd633a1e1acf45992d0e9
Tim Sell <trsell@gmail.com>
2015-06-14 05:14:51 -0400

    gitignore, bla

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var server = require('./api-fake.js');
+var server = require('./api.js');
 
 server.listen(3000, function () {
-  console.log('API fake listening on port %d', server.address().port);
+  console.log('API listening on port %d', server.address().port);
 });
 
